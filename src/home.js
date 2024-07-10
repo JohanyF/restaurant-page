7f54ce2dabe447ced97ebab7a68d3cced83cd9e1
JohanyF <johanyf627@gmail.com>
2024-07-10 02:37:21 -0500

    Added file used to load the home page

---
+++ src/home.js
@@ -0,0 +1,20 @@
+
+export default function setUpHomePage() {
+    const main = document.querySelector('.main');
+
+    const introduction = document.createElement('div');
+    introduction.classList.add('introduction');
+
+    const introText = document.createElement('h1');
+    introText.textContent = 'Bienvenidos'
+    introText.classList.add('intro-text');
+
+    const text = document.createElement('p');
+    text.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate sapien nec sagittis aliquam malesuada.'
+    text.classList.add('text');
+
+    main.appendChild(introduction);
+
+    introduction.appendChild(introText);
+    introduction.appendChild(text);
+}

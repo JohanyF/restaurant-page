7870264ac57d5e3f7c5e00cadc61d53abb8b890b
JohanyF <johanyf627@gmail.com>
2024-07-12 02:47:24 -0500

    Added functionality to each of the nav buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import './style.css';
+import setUpHomePage from './home.js';
+import setUpMenuPage from './menu.js';
+import setUpAboutPage from './about.js';
 
-console.log("TESTING");
+setUpHomePage();
+
+const removeMainChild = () => {
+    const main = document.querySelector('.main');
+    const child = main.lastElementChild;
+    child.remove();
+}
+
+const homeBtn = document.querySelector('#home');
+
+homeBtn.addEventListener('click', () => {
+    removeMainChild();
+    setUpHomePage();
+})
+const menuBtn = document.querySelector('#menu');
+
+menuBtn.addEventListener('click', () => {
+    removeMainChild();
+    setUpMenuPage();
+})
+
+const aboutBtn = document.querySelector('#about');
+
+aboutBtn.addEventListener('click', () => {
+    removeMainChild();
+    setUpAboutPage();
+})
